ba2c16af9ff9513febc61eaae0d7289c79bf0042
Jeka Shiyan <ncringe31@gmail.com>
2022-08-23 14:03:43 +0300

    tabs

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,10 +1,16 @@
-import {Tabs} from '../vendor/tabs'
-
 document.addEventListener("DOMContentLoaded", () => {
 
-  const tabs = document.querySelector(".tabs")
+  const tabsDropdownBtn = document.querySelector('.tabs-dropdown__toggle');
+  const tabsDropdown = document.querySelector('.tabs-dropdown');
 
-  if(tabs) {
-    new Tabs('tab');
-  }
+  tabsDropdownBtn?.addEventListener("click", e => {
+
+    const target = e.currentTarget.innerHTML;
+
+    const setTarget = (str) => e.currentTarget.innerHTML = str
+
+    target === 'Показать фильтры' ? setTarget('Скрыть фильтры') : setTarget('Показать фильтры')
+
+    tabsDropdown.classList.toggle("active")
+  })
 })
--- src/js/vendor/tabs.js
+++ src/js/vendor/tabs.js
@@ -35,27 +35,6 @@ export class Tabs {
   }
 
   init() {
-    this.tabList.setAttribute('role', 'tablist');
-
-    this.tabsBtns.forEach((el, i) => {
-      el.setAttribute('role', 'tab');
-      el.setAttribute('tabindex', '-1');
-      el.setAttribute('id', `${this.selector}${i + 1}`);
-      el.classList.remove('tabs__nav-btn--active');
-    });
-
-    this.tabsPanels.forEach((el, i) => {
-      el.setAttribute('role', 'tabpanel');
-      el.setAttribute('tabindex', '-1');
-      el.setAttribute('aria-labelledby', this.tabsBtns[i].id);
-      el.classList.remove('tabs__panel--active');
-    });
-
-    this.tabsBtns[0].classList.add('tabs__nav-btn--active');
-    this.tabsBtns[0].removeAttribute('tabindex');
-    this.tabsBtns[0].setAttribute('aria-selected', 'true');
-    this.tabsPanels[0].classList.add('tabs__panel--active');
-
     this.tabsDropdownBtn?.addEventListener("click", e => {
 
       const target = e.currentTarget.innerHTML;
@@ -66,62 +45,5 @@ export class Tabs {
 
       this.tabsDropdown.classList.toggle("active")
     })
-  }
-
-  events() {
-    this.tabsBtns.forEach((el, i) => {
-      el.addEventListener('click', (e) => {
-        let currentTab = this.tabList.querySelector('[aria-selected]');
-        this.tabsDropdown?.classList.remove("active")
-
-        if (e.currentTarget !== currentTab) {
-          this.switchTabs(e.currentTarget, currentTab);
-        }
-      });
-
-      el.addEventListener('keydown', (e) => {
-        let index = Array.prototype.indexOf.call(this.tabsBtns, e.currentTarget);
-
-        let dir = null;
-
-        if (e.which === 37) {
-          dir = index - 1;
-        } else if (e.which === 39) {
-          dir = index + 1;
-        } else if (e.which === 40) {
-          dir = 'down';
-        } else {
-          dir = null;
-        }
-
-        if (dir !== null) {
-          if (dir === 'down') {
-            this.tabsPanels[i].focus();
-          } else if (this.tabsBtns[dir]) {
-            this.switchTabs(this.tabsBtns[dir], e.currentTarget);
-          }
-        }
-      });
-    });
-  }
-
-  switchTabs(newTab, oldTab = this.tabs.querySelector('[aria-selected]')) {
-    newTab.focus();
-    newTab.removeAttribute('tabindex');
-    newTab.setAttribute('aria-selected', 'true');
-
-    oldTab.removeAttribute('aria-selected');
-    oldTab.setAttribute('tabindex', '-1');
-
-    let index = Array.prototype.indexOf.call(this.tabsBtns, newTab);
-    let oldIndex = Array.prototype.indexOf.call(this.tabsBtns, oldTab);
-
-    this.tabsPanels[oldIndex].classList.remove('tabs__panel--active');
-    this.tabsPanels[index].classList.add('tabs__panel--active');
-
-    this.tabsBtns[oldIndex].classList.remove('tabs__nav-btn--active');
-    this.tabsBtns[index].classList.add('tabs__nav-btn--active');
-
-    this.options.isChanged(this);
   }
 }
